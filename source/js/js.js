28d473d0b69d0f072b78079dcdc0773effd33256
yberpoc <70940212+yberpoc@users.noreply.github.com>
2021-05-07 22:43:10 +0500

    /update

--- source/js/js.js
+++ source/js/js.js
@@ -1,24 +1,3 @@
-function сlick() {
-    let userClick = document.querySelector('.user');
-    let dropMenuClick = document.querySelector('.drop-menu');
-
-    userClick.onclick = () => {
-        dropMenuClick.classList.toggle('d-block');
-    }
-}
-
-сlick();
-
-function getBurger() {
-    document.querySelector('.burger-menu').onclick = () => {
-        document.querySelector('.burger-menu').classList.toggle('active');
-        document.querySelector('.list').classList.toggle('active');
-        document.querySelector('body').classList.toggle('lock');
-        document.querySelector('.wrapper').classList.toggle('black')
-    }
-}
-
-getBurger();
 
 
 
